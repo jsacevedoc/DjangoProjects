3c0b63b1982b4a2ba4ed2c7cece9cd6529db86da
jsacevedoc <jsacevedoc@eafit.edu.co>
2020-03-03 16:50:03 -0500

    Taller terminado

---
+++ temphum/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Temphum
+from .serializers import TemphumSerializer
+
+class TemphumViewSet(viewsets.ModelViewSet):
+    queryset = Temphum.objects.all().order_by('-created')
+    serializer_class = TemphumSerializer
