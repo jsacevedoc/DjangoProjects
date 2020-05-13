2c4b9829d230db8c9ed8afcdccfd175def54746d
jsacevedoc <jsacevedoc@eafit.edu.co>
2020-05-13 09:32:48 -0500

    Taller 3 primera parte

--- proyectointegrador1/settings.py
+++ proyectointegrador1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'proyectointegrador1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'temphum',
+        'USER': 'root',
+        'PASSWORD': 'carechimba:Q1',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
